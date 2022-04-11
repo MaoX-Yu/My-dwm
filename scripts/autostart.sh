e5d1227df44dce3dda0fef9be5635acd6c1fe943
MaoX-Yu <maoxytz@163.com>
2022-04-11 17:03:21 +0800

    change autostart.sh

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -9,7 +9,7 @@ fcitx5 &
 
 export QT_QPA_PLATFORMTHEME=qt5ct &
 
-feh --bg-fill --no-fehbg -z ~/wallpaper/598673.jpg
+feh --bg-fill --no-fehbg -z ~/wallpaper/598673.jpg &
 
 picom &
 nitrogen --restore &
